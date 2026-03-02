169c6ea92cc1dd6a80f00901c9ec30e7303a9541
Your Name <abhinavsaxena1245@gmail.com>
2026-03-02 08:55:57 +0530

    Yourhkwhsajkshdnkjsahdnkszdhnku message

--- db.js
+++ db.js
@@ -1,18 +1,1 @@
-const mysql = require('mysql2');
-
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'my_database'
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-  } else {
-    console.log("MySQL Connected!");
-  }
-});
-
-module.exports = db;
+ 
